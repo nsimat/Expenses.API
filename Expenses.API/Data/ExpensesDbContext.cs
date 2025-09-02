e033bdcbff8d83849d560fac7ffcf3e70f3b9bee
nsimat <nsimba_matondo@yahoo.fr>
2025-09-02 06:33:08 +0200

    Adding ExpensesDbContext, the application DbContext

---
+++ Expenses.API/Data/ExpensesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Expenses.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expenses.API.Data;
+
+public class ExpensesDbContext: DbContext
+{
+    public ExpensesDbContext(DbContextOptions<ExpensesDbContext> options): base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+}
