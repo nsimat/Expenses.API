80998a661274f06bde1f5483a5e8c778cd9c4f50
nsimat <nsimba_matondo@yahoo.fr>
2025-09-03 11:56:53 +0200

    Adding Create operation for Transaction

---
+++ Expenses.API/Controllers/TransactionsController.cs
@@ -0,0 +1,39 @@
+﻿using Expenses.API.Data;
+using Expenses.API.Dtos;
+using Expenses.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expenses.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TransactionsController : ControllerBase
+{
+    private readonly ILogger<TransactionsController> _logger;
+    private readonly ExpensesDbContext _expensesDbContext;
+
+    public TransactionsController(ExpensesDbContext expensesDbContext, ILogger<TransactionsController> logger)
+    {
+        _expensesDbContext = expensesDbContext ?? throw new ArgumentNullException(nameof(expensesDbContext));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+    [HttpPost]
+    public IActionResult CreateTransaction([FromBody] TransactionDto payload)
+    {
+        _logger.LogInformation("Creating a new transaction...");
+        
+        var newTransaction = new Transaction
+        {
+            Type = payload.Type,
+            Amount = payload.Amount,
+            Category = payload.Category,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow
+        };
+        
+        _expensesDbContext.Transactions.Add(newTransaction);
+        _expensesDbContext.SaveChanges();
+        
+        return Ok("Transaction created!");
+    }
+}
