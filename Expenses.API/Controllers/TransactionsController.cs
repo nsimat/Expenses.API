42bdc1fe6aca4a89e4d4214df7666358671aa921
nsimat <nsimba_matondo@yahoo.fr>
2025-09-03 17:43:24 +0200

    Adding the Update endpoint to the Web API

--- Expenses.API/Controllers/TransactionsController.cs
+++ Expenses.API/Controllers/TransactionsController.cs
@@ -18,7 +18,7 @@ public class TransactionsController : ControllerBase
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
-    [HttpGet]
+    [HttpGet("All")]
     public IActionResult GetAllTransactions()
     {
         _logger.LogInformation("Fetching all transactions...");
@@ -26,8 +26,22 @@ public class TransactionsController : ControllerBase
         var transactions = _expensesDbContext.Transactions.ToList();
         return Ok(transactions);
     }
-    [HttpPost]
-    public IActionResult CreateTransaction([FromBody] TransactionDto payload)
+
+    [HttpGet("Details/{id}")]
+    public IActionResult GetTransactionById(int id)
+    {
+        _logger.LogInformation("Fetching transaction with ID: {Id}...", id);
+        
+        var transaction = _expensesDbContext.Transactions.Find(id);
+        
+        if (transaction == null)
+            return NotFound("Transaction not found!");
+        
+        return Ok(transaction);
+    }
+    
+    [HttpPost("Create")]
+    public IActionResult CreateTransaction([FromBody] PostTransactionDto payload)
     {
         _logger.LogInformation("Creating a new transaction...");
         
@@ -44,5 +58,26 @@ public class TransactionsController : ControllerBase
         _expensesDbContext.SaveChanges();
         
         return Ok("Transaction created!");
+    }
+    
+    [HttpPut("Update/{id}")]
+    public IActionResult UpdateTransaction(int id, [FromBody] PutTransactionDto payload)
+    {
+        _logger.LogInformation("Updating transaction with ID: {Id}...", id);
+        
+        var existingTransaction = _expensesDbContext.Transactions.Find(id);
+        
+        if (existingTransaction == null)
+            return NotFound("Transaction not found!");
+        
+        existingTransaction.Type = payload.Type;
+        existingTransaction.Amount = payload.Amount;
+        existingTransaction.Category = payload.Category;
+        existingTransaction.UpdatedAt = DateTime.UtcNow;
+
+        _expensesDbContext.Update(existingTransaction);
+        _expensesDbContext.SaveChanges();
+        
+        return Ok("Transaction updated!");
     }
 }
