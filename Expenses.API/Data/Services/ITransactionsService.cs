c0fba7a18c51787b9291b9f89a55ca229eb20702
nsimat <nsimba_matondo@yahoo.fr>
2025-09-03 20:24:57 +0200

    Adding TransactionsService to factorize TransactionController

---
+++ Expenses.API/Data/Services/ITransactionsService.cs
@@ -0,0 +1,13 @@
+﻿using Expenses.API.Dtos;
+using Expenses.API.Models;
+
+namespace Expenses.API.Data.Services;
+
+public interface ITransactionsService
+{
+    List<Transaction> GetAll();
+    Transaction? GetById(int id);
+    void Add(PostTransactionDto transaction);
+    Transaction? Update(int id, PutTransactionDto transaction);
+    void Delete(int id);
+}
---
+++ Expenses.API/Data/Services/TransactionsService.cs
@@ -0,0 +1,62 @@
+﻿using Expenses.API.Dtos;
+using Expenses.API.Models;
+
+namespace Expenses.API.Data.Services;
+
+public class TransactionsService(ExpensesDbContext expensesDbContext): ITransactionsService
+{
+    public List<Transaction> GetAll()
+    {
+        var transactions = expensesDbContext.Transactions.ToList();
+        return transactions;
+    }
+
+    public Transaction? GetById(int id)
+    {
+        var transaction = expensesDbContext.Transactions.Find(id);
+        return transaction;
+    }
+
+    public void Add(PostTransactionDto transaction)
+    {
+        var newTransaction = new Transaction
+        {
+            Type = transaction.Type,
+            Amount = transaction.Amount,
+            Category = transaction.Category,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow
+        };
+        
+        expensesDbContext.Transactions.Add(newTransaction);
+        expensesDbContext.SaveChanges();
+    }
+
+    public Transaction? Update(int id, PutTransactionDto transaction)
+    {
+        var existingTransaction = expensesDbContext.Transactions.Find(id);
+
+        if (existingTransaction != null)
+        {
+            existingTransaction.Type = transaction.Type;
+            existingTransaction.Amount = transaction.Amount;
+            existingTransaction.Category = transaction.Category;
+            existingTransaction.UpdatedAt = DateTime.UtcNow;
+
+            expensesDbContext.Update(existingTransaction);
+            expensesDbContext.SaveChanges();
+        }
+        return existingTransaction;
+    }
+
+    public void Delete(int id)
+    {
+        var existingTransaction = expensesDbContext.Transactions.Find(id);
+
+        if (existingTransaction != null)
+        {
+            expensesDbContext.Transactions.Remove(existingTransaction);
+            expensesDbContext.SaveChanges(); 
+        }
+    }
+}
