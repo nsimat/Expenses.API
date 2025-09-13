327e5877a06ed7297e90242faadd383f4bf533d2
nsimat <nsimba_matondo@yahoo.fr>
2025-09-13 22:57:22 +0200

    Adding CORS integration to the API

--- Expenses.API/Program.cs
+++ Expenses.API/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container, especially the DbContext and SQL Server support.
 builder.Services.AddDbContext<ExpensesDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("ExpensesConnection"));
@@ -16,6 +16,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options => 
+    options.AddPolicy(name: "AngularExpensesPolicy",
+        cfg =>
+        {
+            cfg.AllowAnyHeader();
+            cfg.AllowAnyMethod();
+            cfg.WithOrigins(builder.Configuration["AllowedCORS"] ?? "https://localhost:4200");
+        }));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,5 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AngularExpensesPolicy");
+
 app.MapControllers();
 app.Run();
