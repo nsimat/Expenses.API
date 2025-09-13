a0a0b6ad63dd8fbc859ebf7c6d05a9d97545f7e4
nsimat <nsimba_matondo@yahoo.fr>
2025-09-13 18:34:38 +0200

    Adding icons in the navbar

--- Expenses.Frontend/src/app/components/transaction-list/transaction-list.ts
+++ Expenses.Frontend/src/app/components/transaction-list/transaction-list.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Itransaction } from '../../models/itransaction';
+import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-transaction-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './transaction-list.html',
   styleUrl: './transaction-list.css'
 })
 export class TransactionList {
+
+  transactions: Itransaction[] = [
+    { id: 1, type: 'Expense', amount: 50, category: 'Food', createdAt: new Date(), updateAt: new Date() },
+    { id: 2, type: 'Income', amount: 1000, category: 'Salary', createdAt: new Date(), updateAt: new Date() },
+    { id: 3, type: 'Expense', amount: 20, category: 'Transport', createdAt: new Date(), updateAt: new Date() }
+  ];
 
 }
