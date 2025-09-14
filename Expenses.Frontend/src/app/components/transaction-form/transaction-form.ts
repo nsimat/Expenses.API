32e580fae9072c15192c17032d2d0d09156e97bb
nsimat <nsimba_matondo@yahoo.fr>
2025-09-14 12:55:06 +0200

    Implementing the transaction form

--- Expenses.Frontend/src/app/components/transaction-form/transaction-form.ts
+++ Expenses.Frontend/src/app/components/transaction-form/transaction-form.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-transaction-form',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './transaction-form.html',
-  styleUrl: './transaction-form.css'
+  styleUrl: './transaction-form.css',
 })
-export class TransactionForm {
+export class TransactionForm implements OnInit {
+  transactionForm: FormGroup;
 
+  incomeCategories = [
+    'Salary',
+    'Business',
+    'Investment',
+    'Gift',
+    'Other'
+  ];
+
+  expenseCategories = [
+    'Food',
+    'Rent',
+    'Transportation',
+    'Utilities',
+    'Entertainment',
+    'Travel',
+    'Health',
+    'Other',
+  ];
+
+  availableCategories: string[] = [];
+
+  constructor(private fb: FormBuilder) {
+    this.transactionForm = this.fb.group({
+      type: ['Expense', Validators.required],
+      category: ['', Validators.required],
+      amount: ['', [Validators.required, Validators.min(0)]],
+      createdAt: [new Date(), Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+    const type = this.transactionForm.get('type')?.value;
+    this.availableCategories = type === 'Expense' ? this.expenseCategories : this.incomeCategories;
+    this.transactionForm.patchValue({ category: ' ' });
+  }
+
+  cancel() {
+    throw new Error('Method not implemented.');
+  }
+
+  onTypeChange() {
+    ;
+  }
+
+  onSubmit() {
+    throw new Error('Method not implemented.');
+  }
 }
