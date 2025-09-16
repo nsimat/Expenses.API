e8abe1caf1eb983a325e8bcc9bd51e912a425645
nsimat <nsimba_matondo@yahoo.fr>
2025-09-16 22:17:16 +0200

    Configure token-based authentication  in the ASP.NET Core Web API

--- Expenses.API/Program.cs
+++ Expenses.API/Program.cs
@@ -1,6 +1,8 @@
 using Expenses.API.Data;
 using Expenses.API.Data.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +27,25 @@ builder.Services.AddCors(options =>
             cfg.WithOrigins(builder.Configuration["AllowedCORS"] ?? "https://localhost:4200");
         }));
 
+// Properly set up the JwtBearerMiddleware 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        
+        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+        ValidAudience = builder.Configuration["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+            builder.Configuration["JwtSettings:SecurityKey"] ??
+            throw new InvalidOperationException("JWT Key not found in configuration."))),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -34,9 +55,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AngularExpensesPolicy");
+
 app.UseHttpsRedirection();
 
-app.UseCors("AngularExpensesPolicy");
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 app.Run();
