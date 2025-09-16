a8d04c11f0059d763c940303010ffb8e774f5ce4
nsimat <nsimba_matondo@yahoo.fr>
2025-09-16 22:32:33 +0200

    Adding NgOptimizedImage for logo image integration

--- Expenses.Frontend/src/app/components/header/header.ts
+++ Expenses.Frontend/src/app/components/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, NgOptimizedImage],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
