4867296a99578631972a25c510b212d89886b47a
nsimat <nsimba_matondo@yahoo.fr>
2025-10-04 17:32:17 +0200

    Add validation to UserVreationDto

---
+++ Expenses.API/Dtos/UserCreationDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Expenses.API.Dtos;
+
+public class UserCreationDto
+{
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email format!")]
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "Password is required.")]
+    [MinLength(6, ErrorMessage = "Password must be at least 6 characters long!")]
+    public string Password { get; set; }
+}
