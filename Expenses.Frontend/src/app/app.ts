213b554d14e75a971bc9e1a13b9ca543bf65677d
nsimat <nsimba_matondo@yahoo.fr>
2025-10-05 23:13:47 +0200

    Redesign main application page to display a dashboard

--- Expenses.Frontend/src/app/app.ts
+++ Expenses.Frontend/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 import { TransactionList } from './components/transaction-list/transaction-list';
+import {Sidebar} from './components/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
