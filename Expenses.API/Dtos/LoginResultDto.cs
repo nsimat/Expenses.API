847dbc1d068955fa7c5090d06d58f68e3d05e467
nsimat <nsimba_matondo@yahoo.fr>
2025-10-05 11:33:08 +0200

    Add DTO class UserLoginDto for login request

---
+++ Expenses.API/Dtos/LoginResultDto.cs
@@ -0,0 +1,22 @@
+﻿namespace Expenses.API.Dtos;
+
+/// <summary>
+/// Strongly typed result class to inform the client of a login attempt result, sending it the JWT if successful.
+/// </summary>
+public class LoginResultDto
+{
+    /// <summary>
+    /// TRUE if the login attempt is successful, FALSE otherwise.
+    /// </summary>
+    public bool Success { get; set; }
+    
+    /// <summary>
+    /// Login attempt result message.
+    /// </summary>
+    public required string Message { get; set; }
+    
+    /// <summary>
+    /// The JWT token generated for the user if the login attempt is successful, NULL otherwise.
+    /// </summary>
+    public string? Token { get; set; }
+}
