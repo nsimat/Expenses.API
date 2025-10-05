6fd6abd310d573fffd4ee18a98c571de3463af08
nsimat <nsimba_matondo@yahoo.fr>
2025-10-05 23:16:16 +0200

    Add the sidebar component

---
+++ Expenses.Frontend/src/app/components/sidebar/sidebar.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faBookmark,
+  faChartBar,
+  faComment,
+  faHome,
+  faMoneyBillTransfer,
+  faUser
+} from '@fortawesome/free-solid-svg-icons';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [FontAwesomeModule, RouterLink],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.css'
+})
+export class Sidebar {
+  home = faHome;
+  chart = faChartBar;
+  message = faComment;
+  bookmark = faBookmark;
+  user = faUser;
+  transaction = faMoneyBillTransfer;
+
+}
