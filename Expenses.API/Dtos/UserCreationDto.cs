1ef523fa485485ca7efc34aa6bd3d99d02eb75ea
nsimat <nsimba_matondo@yahoo.fr>
2025-10-05 05:21:38 +0200

    Add required attribute to properties of UserCreationDto

--- Expenses.API/Dtos/UserCreationDto.cs
+++ Expenses.API/Dtos/UserCreationDto.cs
@@ -2,13 +2,16 @@
 
 namespace Expenses.API.Dtos;
 
+/// <summary>
+/// This DTO aims to receive the user's credentials for account creation from the client.
+/// </summary>
 public class UserCreationDto
 {
-    [Required(ErrorMessage = "Email is required.")]
     [EmailAddress(ErrorMessage = "Invalid email format!")]
-    public string Email { get; set; }
+    [Required(ErrorMessage = "Email is required.")]
+    public required string Email { get; set; }
     
-    [Required(ErrorMessage = "Password is required.")]
     [MinLength(6, ErrorMessage = "Password must be at least 6 characters long!")]
-    public string Password { get; set; }
+    [Required(ErrorMessage = "Password is required.")]
+    public required string Password { get; set; }
 }
