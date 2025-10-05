61ed57aceb66a446fd1c588cc4ab1771598f8d2a
nsimat <nsimba_matondo@yahoo.fr>
2025-10-05 11:37:49 +0200

    Add Login DTO for request treatment

---
+++ Expenses.API/Dtos/UserLoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Expenses.API.Dtos;
+
+public class UserLoginDto
+{
+    [EmailAddress(ErrorMessage = "Invalid email format!")]
+    [Required(ErrorMessage = "Email is required.")]
+    public required string Email { get; set; }
+    
+    [MinLength(6, ErrorMessage = "Password must be at least 6 characters long!")]
+    [Required(ErrorMessage = "Password is required.")]
+    public required string Password { get; set; }
+}
