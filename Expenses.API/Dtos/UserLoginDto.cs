f918ba970bc3f59e94cf262c5b38fd076e63cec4
nsimat <nsimba_matondo@yahoo.fr>
2025-10-07 23:27:39 +0200

    Add documentation to UserLoginDto fields

--- Expenses.API/Dtos/UserLoginDto.cs
+++ Expenses.API/Dtos/UserLoginDto.cs
@@ -2,12 +2,21 @@
 
 namespace Expenses.API.Dtos;
 
+/// <summary>
+/// A DTO to receive user credentials for login from the client.
+/// </summary>
 public class UserLoginDto
 {
+    /// <summary>
+    /// A valid email address is required.
+    /// </summary>
     [EmailAddress(ErrorMessage = "Invalid email format!")]
     [Required(ErrorMessage = "Email is required.")]
     public required string Email { get; set; }
     
+    /// <summary>
+    /// A password with a minimum length of 6 characters is required.
+    /// </summary>
     [MinLength(6, ErrorMessage = "Password must be at least 6 characters long!")]
     [Required(ErrorMessage = "Password is required.")]
     public required string Password { get; set; }
