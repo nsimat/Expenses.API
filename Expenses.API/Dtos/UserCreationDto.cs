362bf6d88fdd12641e3cfa5612d57f10950768e8
nsimat <nsimba_matondo@yahoo.fr>
2025-10-13 01:57:38 +0200

    Add xml comments to UserCreationDto properties

--- Expenses.API/Dtos/UserCreationDto.cs
+++ Expenses.API/Dtos/UserCreationDto.cs
@@ -7,10 +7,16 @@ namespace Expenses.API.Dtos;
 /// </summary>
 public class UserCreationDto
 {
+    /// <summary>
+    /// Email to be used as username, must be in a valid email format
+    /// </summary>
     [EmailAddress(ErrorMessage = "Invalid email format!")]
     [Required(ErrorMessage = "Email is required.")]
     public required string Email { get; set; }
     
+    /// <summary>
+    /// Password to be used for authentication, must be at least 6 characters long
+    /// </summary>
     [MinLength(6, ErrorMessage = "Password must be at least 6 characters long!")]
     [Required(ErrorMessage = "Password is required.")]
     public required string Password { get; set; }
