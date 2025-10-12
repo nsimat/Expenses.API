b3385a47b3b3979c5c6f42f4e56d8fc96601f3f7
nsimat <nsimba_matondo@yahoo.fr>
2025-10-13 01:58:43 +0200

    Add xml comments to UserLoginDto properties

--- Expenses.API/Dtos/UserLoginDto.cs
+++ Expenses.API/Dtos/UserLoginDto.cs
@@ -8,14 +8,14 @@ namespace Expenses.API.Dtos;
 public class UserLoginDto
 {
     /// <summary>
-    /// A valid email address is required.
+    /// A valid email address is required for username authentication.
     /// </summary>
     [EmailAddress(ErrorMessage = "Invalid email format!")]
     [Required(ErrorMessage = "Email is required.")]
     public required string Email { get; set; }
     
     /// <summary>
-    /// A password with a minimum length of 6 characters is required.
+    /// A password with a minimum length of 6 characters is required for password authentication.
     /// </summary>
     [MinLength(6, ErrorMessage = "Password must be at least 6 characters long!")]
     [Required(ErrorMessage = "Password is required.")]
