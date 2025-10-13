434a7ab60df733a5db789d692a4aab55c31238c5
nsimat <nsimba_matondo@yahoo.fr>
2025-10-13 02:08:54 +0200

    Refactor signup form validation and submission logic

--- Expenses.Frontend/src/app/components/signup/signup.ts
+++ Expenses.Frontend/src/app/components/signup/signup.ts
@@ -1,5 +1,12 @@
 import {Component, inject} from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators
+} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import {AuthService} from '../../services/auth-service';
 import {User} from '../../models/user';
@@ -23,36 +30,32 @@ export class Signup {
   private readonly router = inject(Router);
   protected readonly signupForm = this.formBuilder.group(
     {
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
-    },
-    {
-      validator: this.passwordMatchValidator
+      email: [''!, [Validators.required, Validators.email]],
+      password: [''!, [Validators.required, Validators.minLength(6)]],
+      confirmPassword: [''!, [Validators.required, Validators.minLength(6), Signup.passwordMatch]],
     }
   );
 
+  // Method to check if a form control has a specific error and has been touched or dirty
   hasError(controlName: string, errorName: string): boolean {
     const control = this.signupForm.get(controlName);
     return (control?.touched || control?.dirty) && control?.hasError(errorName) || false;
   }
 
-  private passwordMatchValidator(form: FormGroup) {
-    return form.get('password')?.value === form.get('confirmPassword')?.value
-      ? null
-      : {'passwordMissMatch': true};
+  // Custom validator to check if password and confirmPassword match
+  private  static passwordMatch(control: AbstractControl<string>): ValidationErrors | null {
+    const formGroup = control.parent as FormGroup;
+    return formGroup?.get('password')?.value === control?.value ? null : {matchingError: true};
   }
 
-  protected register(): void {
-  }
-
-  onSubmit(loginRequest: User): void {
+  // Method for handling form submission when the user clicks the "Sign Up" button
+  register(): void {
     console.log('Submitting form with values:', this.signupForm.value);
     this.errorMessage = null; // Clear previous error messages
     if (this.signupForm.valid) {
-      const newUser = this.signupForm.value;
-      console.log('Form submitted is valid:', loginRequest);
-      this.apiService.register(newUser).subscribe(
+      const signUpData = this.signupForm.value;
+      console.log('Form submitted is valid:', signUpData);
+      this.apiService.register(<User>signUpData).subscribe(
         {
           next: (loginResult) => {
             this.result = loginResult;// Store the result for further processing???
@@ -65,16 +68,16 @@ export class Signup {
           error: (error) => {
             console.log('Signup failed. Check the server error:', error.error?.message[0]);
             this.errorMessage = error.error?.message || 'An unknown error occurred during signup. Please try again.';
-            console.log(error);// test
             if(error.status == 400){
-              this.result = error.error?.message[0] || 'Bad Request';
-              this.router.navigate(['/signup']);}
+              this.router.navigate(['/signup']);
+            }
           }
         }
       )
     } else {
       // Form is invalid, display form validation errors to the user
       console.log('Form submitted is invalid:', this.signupForm.errors);
+      this.router.navigate(['/signup']);
     }
   }
 }
