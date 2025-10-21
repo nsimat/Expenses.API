e26f34080c2a004827ad3532f95fe3f64d75683c
nsimat <nsimba_matondo@yahoo.fr>
2025-10-21 21:15:48 +0200

    Add guards to routes

--- Expenses.Frontend/src/app/app.routes.ts
+++ Expenses.Frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Signup } from './components/signup/signup';
 import { TransactionList } from './components/transaction-list/transaction-list';
 import { TransactionForm } from './components/transaction-form/transaction-form';
 import {PageNotFound} from './components/page-not-found/page-not-found';
+import {authGuard} from './guards/auth-guard';
 
 export const routes: Routes = [
   // Route mapping the URL '/login' to Login component
@@ -20,22 +21,26 @@ export const routes: Routes = [
   {
     path: 'transactions',
     component: TransactionList,
+    canActivate: [authGuard]
   },
   // Route mapping the URL '/add' to TransactionForm component
   {
     path: 'add',
     component: TransactionForm,
+    canActivate: [authGuard]
   },
   // Route mapping the URL '/edit/:id' to TransactionForm component
   {
     path: 'edit/:id',
     component: TransactionForm,
+    canActivate: [authGuard]
   },
   // Route redirecting the root URL '' to the '/transactions' route
   {
     path: '',
     redirectTo: '/transactions',
     pathMatch: 'full',
+    //canActivate: [authGuard]
   },
   // Redirecting all unmatched URLs to the '404 Page'; Catching all to prevent errors
   {
