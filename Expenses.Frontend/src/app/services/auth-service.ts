ae75f17795da21649b3310026194547454c21cfa
nsimat <nsimba_matondo@yahoo.fr>
2025-10-21 21:14:36 +0200

    Enhance authentication service with user email management and status tracking

--- Expenses.Frontend/src/app/services/auth-service.ts
+++ Expenses.Frontend/src/app/services/auth-service.ts
@@ -1,35 +1,50 @@
 import { LoginResult } from '../models/login-result';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpResponse} from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
-import { User } from '../models/user';
-import { Observable, tap } from 'rxjs';
+import { LoginRequest } from '../models/login-request';
+import {BehaviorSubject, Observable, tap} from 'rxjs';
 import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-
   // Dependency Injections using inject() function
   private readonly http = inject(HttpClient);
   private readonly apiAuthUrl = environment.apiAuthUrl;
   private readonly router = inject(Router);
 
-  public tokenKey: string = "token";
+  // Key to store the token in local storage
+  private tokenKey: string = "token";
+  // Public field to receive the email of the logged-in user
+  private userEmail: string = 'userEmail';
+  // BehaviorSubject to manage authentication status
+  private _authStatus = new BehaviorSubject<boolean>(false);
+  public authStatus$ = this._authStatus.asObservable();
 
   // check if the user is authenticated by checking if the token exists in local storage
   isAuthenticated(): boolean {
     return this.getToken() !== null; // Returns true if token exists, false otherwise
   }
 
+  // get the user email
+  getUserEmail(): string | null {
+    return localStorage.getItem('userEmail');
+  }
   // Retrieve the token from local storage
   getToken(): string | null {
     return localStorage.getItem(this.tokenKey);
   }
 
+  // Initialize the authentication status on service creation
+  initializeAuthStatus(): void {
+    const isAuth = this.isAuthenticated();
+    this.setAuthStatus(isAuth);
+  }
+
   // Authenticate the user and store the token
-  login(credentials: User): Observable<LoginResult> {
+  login(credentials: LoginRequest): Observable<LoginResult> {
     console.log('Login in the system...');
     const url = this.apiAuthUrl + '/Login';
 
@@ -38,21 +53,22 @@ export class AuthService {
         if (loginResult.success && loginResult.token) {
           console.log('Login accepted for: ', credentials.email);
           localStorage.setItem(this.tokenKey, loginResult.token);
+          localStorage.setItem(this.userEmail, credentials.email);
+          this.setAuthStatus(true);
         }
       })
     );
   }
 
   // Register by creating a new user and store the token
-  register(credentials: User): Observable<any> {
+  register(credentials: LoginRequest): Observable<any> {
     console.log('Registering user with credentials: ', credentials);
 
     const url = this.apiAuthUrl + '/Register';
-
     return this.http.post<any>(url, credentials).pipe(
       tap((response) => {
         localStorage.setItem(this.tokenKey, response.token);
-        console.log('User accepted with credentials: ', credentials);
+        console.log('LoginRequest accepted with credentials: ', credentials);
       })
     );
   }
@@ -60,8 +76,15 @@ export class AuthService {
   // Logout by clearing the token and navigate to the login page
   logout(): void {
     // Implement logout logic if needed, e.g., clear tokens, notify server, etc.
-    console.log('User logged out...');
+    console.log('LoginRequest logged out...');
     localStorage.removeItem(this.tokenKey);
+    localStorage.removeItem(this.userEmail);
+    this.setAuthStatus(false);
     this.router.navigate(['/login']);
+  }
+
+  // Update the authentication status
+  private setAuthStatus(isAuthenticated: boolean): void {
+    this._authStatus.next(isAuthenticated);
   }
 }
