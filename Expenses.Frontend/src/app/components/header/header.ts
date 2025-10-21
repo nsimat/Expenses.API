30c5d19ecb4c577a68d127f4fe852f4aa4425fa6
nsimat <nsimba_matondo@yahoo.fr>
2025-10-21 21:22:36 +0200

    Implement header component

--- Expenses.Frontend/src/app/components/header/header.ts
+++ Expenses.Frontend/src/app/components/header/header.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
+import {Router, RouterLink} from "@angular/router";
 import {NgOptimizedImage} from '@angular/common';
+import {AuthService} from '../../services/auth-service';
+import {Subject, takeUntil} from 'rxjs';
+
 
 @Component({
   selector: 'app-header',
@@ -8,6 +11,35 @@ import {NgOptimizedImage} from '@angular/common';
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
-export class Header {
+export class Header implements OnInit, OnDestroy {
 
+  private destroySubject = new Subject();
+  isLoggedIn: boolean = false;
+  loggedInUserEmail: string | null = null;
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.authService.authStatus$
+      .pipe(takeUntil(this.destroySubject))
+      .subscribe(result => {
+        this.isLoggedIn = result;
+        this.loggedInUserEmail = result ? this.authService.getUserEmail() : null;
+      });
+  }
+
+  // Method to handle user logout
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  // Initialize the component and set the initial login status
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isAuthenticated();
+  }
+
+  // Clean up subscriptions to prevent memory leaks
+  ngOnDestroy(): void {
+    this.destroySubject.next(true);
+    this.destroySubject.complete();
+  }
 }
