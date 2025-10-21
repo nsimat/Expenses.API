1ccd74a321e90eaef228ddceb0b97883e3a39a5e
nsimat <nsimba_matondo@yahoo.fr>
2025-10-21 20:42:07 +0200

    Add XML comments to Transaction properties

--- Expenses.API/Models/Transaction.cs
+++ Expenses.API/Models/Transaction.cs
@@ -4,7 +4,18 @@ namespace Expenses.API.Models;
 
 public class Transaction: BaseEntity
 {
+    /// <summary>
+    /// Type of the transaction, e.g., "income" or "expense"
+    /// </summary>
     public string Type { get; set; }
+    
+    /// <summary>
+    /// Amount of the transaction
+    /// </summary>
     public double Amount { get; set; }
+    
+    /// <summary>
+    /// Category of the transaction, e.g., "food", "salary", etc.
+    /// </summary>
     public string Category { get; set; }
 }
