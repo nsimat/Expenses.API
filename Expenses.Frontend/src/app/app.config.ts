c02552ae22017e8ec28b045064490bb2f2eefd93
nsimat <nsimba_matondo@yahoo.fr>
2025-10-25 18:00:22 +0200

    Rename error-handler.interceptor.ts

--- Expenses.Frontend/src/app/app.config.ts
+++ Expenses.Frontend/src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {authInterceptor} from './interceptors/auth.interceptor';
-import {errorHandlerInterceptor} from './interceptors/erro-handler.interceptor';
+import {errorHandlerInterceptor} from './interceptors/error-handler.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
