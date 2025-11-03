b8ddf119988ecce86208ce3d4c137547f2decd1c
nsimat <nsimba_matondo@yahoo.fr>
2025-11-03 19:20:07 +0100

    Add a DTO for updating a user

---
+++ Expenses.API/Dtos/UserUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Expenses.API.Dtos;
+
+/// <summary>
+/// A DTO containing part of user data
+/// </summary>
+public class UserUpdateDto
+{
+    // The user email
+    public string email { get; set; }
+    
+    // User first name
+    public string FirstName { get; set; }
+    
+    // User last name
+    public string LastName { get; set; }
+}
