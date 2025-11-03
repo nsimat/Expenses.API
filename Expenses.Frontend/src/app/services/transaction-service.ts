d07de04e2b6a912b19f7e335b5270f73df3e47b2
nsimat <nsimba_matondo@yahoo.fr>
2025-11-03 21:10:10 +0100

    Rename transaction service to transaction-service

--- Expenses.Frontend/src/app/services/transaction-service.ts
+++ Expenses.Frontend/src/app/services/transaction-service.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Itransaction } from '../models/itransaction';
+import { Transaction } from '../models/transaction';
 import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
-export class Transaction {
+export class TransactionService {
 
   //private apiUrl = 'https://localhost:7063/api/Transactions';
   // Alternatively, use environment variables if set up
@@ -16,23 +16,23 @@ export class Transaction {
   constructor(private http: HttpClient) {}
 
   // Retrieve all transactions
-  getAllTransactions(): Observable<Itransaction[]>{
-    return this.http.get<Itransaction[]>(`${this.apiUrl}/All`);
+  getAllTransactions(): Observable<Transaction[]>{
+    return this.http.get<Transaction[]>(`${this.apiUrl}/All`);
   }
 
   // Retrieve a transactions by id
-  getTransactionById(id: number): Observable<Itransaction>{
-    return this.http.get<Itransaction>(`${this.apiUrl}/Details/` + id);
+  getTransactionById(id: number): Observable<Transaction>{
+    return this.http.get<Transaction>(`${this.apiUrl}/Details/` + id);
   }
 
   // Create a new transaction
-  createTransaction(transaction: Itransaction): Observable<Itransaction>{
-    return this.http.post<Itransaction>(this.apiUrl + "/Create", transaction);
+  createTransaction(transaction: Transaction): Observable<Transaction>{
+    return this.http.post<Transaction>(this.apiUrl + "/Create", transaction);
   }
 
   // Update an existing transaction
-  updateTransaction(id: number, transaction: Itransaction): Observable<Itransaction>{
-    return this.http.put<Itransaction>(this.apiUrl + "/Update/" + id, transaction);
+  updateTransaction(id: number, transaction: Transaction): Observable<Transaction>{
+    return this.http.put<Transaction>(this.apiUrl + "/Update/" + id, transaction);
   }
 
   // Delete a transaction by id
