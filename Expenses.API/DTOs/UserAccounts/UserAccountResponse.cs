55032ee6d2672484190af45883d18d58d0d2153d
nsimat <nsimba_matondo@yahoo.fr>
2026-06-20 10:14:09 +0200

    Remove the JsonPropertyName attribute from user account properties

--- Expenses.API/DTOs/UserAccounts/UserAccountResponse.cs
+++ Expenses.API/DTOs/UserAccounts/UserAccountResponse.cs
@@ -22,19 +22,16 @@ public class UserAccountResponse
     /// <summary>
     /// Email of the user
     /// </summary>
-    [JsonPropertyName("email")]
     public required string Email { get; set; } = string.Empty;
  
     /// <summary>
     /// The first name of the user
     /// </summary>
-    [JsonPropertyName("first_name")]
     public string? FirstName { get; set; }
 
     /// <summary>
     /// The last name of the user
     /// </summary>
-    [JsonPropertyName("last_name")]
     public string? LastName { get; set; }
 
     /// <summary>
@@ -50,30 +47,25 @@ public class UserAccountResponse
     /// <summary>
     /// The date of birth of the user
     /// </summary>
-    [JsonPropertyName("date_of_birth")]
     public DateTimeOffset? DateOfBirth { get; set; }
 
     /// <summary>
     /// The date and time when the account was created.
     /// </summary>
-    [JsonPropertyName("created_at")]
     public DateTimeOffset CreatedAt { get; set; }
 
     /// <summary>
     /// The timestamp indicating when the record was last updated.
     /// </summary>
-    [JsonPropertyName("updated_at")]
     public DateTimeOffset UpdatedAt { get; set; }
 
     /// <summary>
     /// List of timestamps representing the most recent login times of the user.
     /// </summary>
-    [JsonPropertyName("recent_login_times")]
     public List<DateTimeOffset>? RecentLoginTimes { get; set; }
 
     /// <summary>
     /// List of transactions associated with the user
     /// </summary>
-    [JsonPropertyName("user_transactions")]
     public ICollection<TransactionResponse> Transactions { get; set; } = new HashSet<TransactionResponse>();
 }
